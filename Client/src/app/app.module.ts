1d2a5248030e198d719085a186ad62d1643af401
DorAmiel <doradir@gmail.com>
2022-06-28 18:35:47 +0300

    init client

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarDownComponent } from './components/navbar/navbar-down/navbar-down.component';
+import { NavbarUpComponent } from './components/navbar/navbar-up/navbar-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    NavbarDownComponent,
+    NavbarUpComponent,
   ],
   imports: [
     BrowserModule,
