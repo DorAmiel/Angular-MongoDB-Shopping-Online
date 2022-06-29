b7b58f671ccfe0efa12269cf0a918342fb9c3a6d
eilon <eilon159123@gmail.com>
2022-06-29 11:48:55 +0300

    css

--- Client/src/app/components/navbar/navbar-down/navbar-down.component.ts
+++ Client/src/app/components/navbar/navbar-down/navbar-down.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarDownComponent implements OnInit {
 
+  arry = [1, 2, 3, 4, 5, 6, 7, 8];
   constructor() { }
 
   ngOnInit(): void {
