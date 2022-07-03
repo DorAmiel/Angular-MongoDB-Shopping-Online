7248ff80a92c5f012fd031caaa4402a4e2030fe3
eilon <eilon159123@gmail.com>
2022-07-03 14:06:05 +0300

    products

--- Client/src/app/components/body/body.component.ts
+++ Client/src/app/components/body/body.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BodyComponent implements OnInit {
   constructor() { }
-array= [1,2,3];
+array= [1,2,3,4,5,6,7,8,9,10];
   ngOnInit(): void {
   }
 
