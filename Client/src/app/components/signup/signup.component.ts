0143cbac56ee3b27ac24b83126b7566f8a0c989d
DorAmiel <doradir@gmail.com>
2022-07-04 13:14:44 +0300

    update

--- Client/src/app/components/signup/signup.component.ts
+++ Client/src/app/components/signup/signup.component.ts
@@ -5,12 +5,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
   cities = ['Jerusalem', 'Tel Aviv','Haifa','Beer Sheva','Ashdod','Rishon LeZion','Rehovot','Netanya','Bat Yam','Ramat Gan','Ashqelon','Ashkelon','Bnei Brak','Eilat','Givatayim','Hadera','Herzliya','Holon','Kfar Saba','Mevo Betar','Netivot',];
+  showNextStep:boolean=true
+  showLastStep:boolean=false
   
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleSteps(){
+   this.showNextStep=!this.showNextStep 
+   this.showLastStep=!this.showLastStep
   }
 
 }
