ee80b049e2e7609089b5b1ceccaddbf04fe14391
eilon <eilon159123@gmail.com>
2022-08-03 11:49:05 +0300

    order-form

--- Client/src/app/pages/create-order/create-order.component.ts
+++ Client/src/app/pages/create-order/create-order.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectCartState } from 'src/app/selectors/cart.selector';
+import { selectUserState } from 'src/app/selectors/user.selector';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-create-order',
@@ -8,6 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class CreateOrderComponent implements OnInit {
 
   cities = ['Jerusalem', 'Tel Aviv', 'Haifa', 'Beer Sheva', 'Ashdod', 'Rishon LeZion', 'Rehovot', 'Netanya', 'Bat Yam', 'Ramat Gan', 'Ashqelon', 'Ashkelon', 'Bnei Brak', 'Eilat', 'Givatayim', 'Hadera', 'Herzliya', 'Holon', 'Kfar Saba', 'Mevo Betar', 'Netivot',];
+
   //today date
   today = new Date();
   year = this.today.getFullYear();
@@ -15,10 +21,27 @@ export class CreateOrderComponent implements OnInit {
   day = this.today.getDate();
   currentDate = ''
 
+  user$: Observable<any>
+  user: any;
 
-  constructor() { }
+  cart: any;
+
+  constructor(private store: Store, private cartService: CartService) {
+    this.user$ = this.store.select(selectUserState)
+    this.user$.subscribe(user => {
+      this.user = user;
+      this.getUserCart(user.cartId);
+    });
+  }
+
 
   ngOnInit(): void {
+    this.setCurrentDate();
+  }
+
+
+  //set current date
+  setCurrentDate() {
     if (this.day < 10 && this.month < 10) {
       this.currentDate = this.year + '-0' + this.month + '-0' + this.day;
     }
@@ -33,4 +56,14 @@ export class CreateOrderComponent implements OnInit {
     }
   }
 
+  async getUserCart(cartId: string): Promise<any> {
+    try {
+      const cart = await this.cartService.getCartById(cartId).toPromise();
+      if (cart) {
+        this.cart = cart;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
